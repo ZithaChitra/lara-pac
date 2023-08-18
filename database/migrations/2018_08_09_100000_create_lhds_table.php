3c2f9da6c521d3ceb8d1cc87d8a4a71f17b739eb
Blessing Chitra <blessing@brandsplash.co.za>
2023-08-18 13:58:28 +0200

    update

--- database/migrations/2018_08_09_100000_create_lhds_table.php
+++ database/migrations/2018_08_09_100000_create_lhds_table.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 
-class CreatePostsTable extends Migration
+class CreateLHDsTable extends Migration
 {
     /**
      * Run the migrations.
